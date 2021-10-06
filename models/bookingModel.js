4c137e61c1e1d05abcd3bc9d1367b7f762f2bf56
Usama Wazir <usamawzr@gmail.com>
2021-10-06 19:37:35 +0500

    a

--- models/bookingModel.js
+++ models/bookingModel.js
@@ -15,7 +15,7 @@ const bookingSchema = new mongoose.Schema({
     },
     price: {
         type: Number,
-        required: [true, 'A booking must have a Tour']
+        // required: [true, 'A booking must have a Tour']
     },
     createdAt: {
         type: Date,
